d2ca75e490a039c0ca2d40def718341842522743
Lucas Coronel <lccoronel7@icloud.com>
2021-05-10 19:58:43 -0300

    feat: add fonts in project

---
+++ reactjs/dtmoney/src/App.tsx
@@ -0,0 +1,10 @@
+import { GlobalStyles } from "./styles/globals";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello Lucas!</h1>
+      <GlobalStyles />
+    </div>
+  );
+};
