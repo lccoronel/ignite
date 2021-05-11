eaf57f3f8aebc325996de0329cb86865a9c88497
Lucas Coronel <lccoronel7@icloud.com>
2021-05-11 17:42:02 -0300

    feat: add components trasactions table

---
+++ reactjs/dtmoney/src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+
+import { Container } from './styles';
+
+export function TransactionsTable() {
+  return (
+    <Container>
+      <table>
+        <thead>
+          <th>Titulo</th>
+          <th>Valor</th>
+          <th>Categoria</th>
+          <th>Data</th>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/12/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="withdraw">- R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+}
