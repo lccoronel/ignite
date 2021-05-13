452502766ae5f515bb51b20d4a92c16d122a2a32
Lucas Coronel <lccoronel7@icloud.com>
2021-05-13 19:10:02 -0300

    feat: modal new trasaction

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyles } from "./styles/globals";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionMOdal() {
+    setIsNewTransactionModalOpen(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionMOdal} />
       <GlobalStyles />
     </>
   );
