46d9250e2c6cd2b28d91e1e8354c5f1f28b70322
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-05-24 08:43:51 -0300

    fix: sign in button  component

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Local | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
       <h1>Ola Lucas</h1>
     </>
