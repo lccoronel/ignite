f5ba295d03602a5cf7d3c88c18e0da9b21ef859d
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-05-24 09:16:21 -0300

    featx: home page

--- reactjs/ignews/src/pages/index.tsx
+++ reactjs/ignews/src/pages/index.tsx
@@ -1,12 +1,26 @@
 import Head from 'next/head';
 
+import  styles from './home.module.scss';
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1>Ola Lucas</h1>
+      
+      <main className={styles.contentConteiner}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
