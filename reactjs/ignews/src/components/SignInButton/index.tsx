3f133f148adef4434d39ff7932afc3d1b6b21fa1
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-05-25 08:08:54 -0300

    feat: next auth integration

--- reactjs/ignews/src/components/SignInButton/index.tsx
+++ reactjs/ignews/src/components/SignInButton/index.tsx
@@ -1,21 +1,31 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signout, useSession } from 'next-auth/client';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const [session] = useSession();
+  console.log(session);
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  return session ? (
+    <button 
+      type="button" 
+      className={styles.signInButton}
+      onClick={() => signout()}
+    >
       <FaGithub color="#04d361" />
 
-      Lucas Coronel
+      {session.user.name}
 
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button 
+      type="button" 
+      className={styles.signInButton} 
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
 
       Sign in with Github
