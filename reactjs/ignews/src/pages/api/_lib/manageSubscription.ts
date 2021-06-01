cfecc209b84348c418db8a178d748b6fa28d40fa
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-06-01 08:53:26 -0300

    feat: listen all webhooks

--- reactjs/ignews/src/pages/api/_lib/manageSubscription.ts
+++ reactjs/ignews/src/pages/api/_lib/manageSubscription.ts
@@ -2,36 +2,55 @@ import { query } from "faunadb";
 import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
 
-export async function saveSubscription(subscriptionId: string, customerId: string ) {
-  try {
-    const userRef = await fauna.query(
-      query.Select(
-        "ref",
-        query.Get(
-          query.Match(
-            query.Index('user_by_stripe_customer_id'),
-            customerId
-          )
+export async function saveSubscription(
+  subscriptionId: string, 
+  customerId: string, 
+  createAction = false
+) {
+  const userRef = await fauna.query(
+    query.Select(
+      "ref",
+      query.Get(
+        query.Match(
+          query.Index('user_by_stripe_customer_id'),
+          customerId
         )
       )
     )
-    
-    const subscription = await stripe.subscriptions.retrieve(subscriptionId); 
+  )
+  
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId); 
 
-    const subscriptionData = {
-      id: subscription.id,
-      userId: userRef,
-      status: subscription.status,
-      price_id: subscription.items.data[0].price.id
-    }; 
+  const subscriptionData = {
+    id: subscription.id,
+    userId: userRef,
+    status: subscription.status,
+    price_id: subscription.items.data[0].price.id
+  }; 
 
+  console.log(subscriptionData);
+
+  if (createAction) {
     await fauna.query(
       query.Create(
         query.Collection('subscriptions'),
         { data: subscriptionData }
       )
     )
-  } catch (err) {
-    console.log(err);
+  } else {
+    await fauna.query(
+      query.Replace(
+        query.Select(
+          "ref",
+          query.Get(
+            query.Match(
+              query.Index('subscription_by_id'),
+              subscriptionId,
+            )
+          )
+        ),
+        { data: subscriptionData }
+      )
+    )
   }
 }
