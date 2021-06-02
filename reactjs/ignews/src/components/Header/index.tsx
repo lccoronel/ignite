5561636c5326a2fb7cce4f146c28b9afd59db18f
Lucas Cardoso <54275445+lccoronel@users.noreply.github.com>
2021-06-02 18:04:24 -0300

    feat: active link to navigate on app in header

--- reactjs/ignews/src/components/Header/index.tsx
+++ reactjs/ignews/src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
@@ -8,8 +10,12 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a className={styles.active}>Nome</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Nome</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
